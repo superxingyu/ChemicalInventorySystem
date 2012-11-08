101233b9b132599360f3f3493ffcb32629c3d46f
superxingyu <superxingyu@gmail.com>
2012-11-08 10:48:43 -0800

    created project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChemicalInventorySystem::Application.config.secret_token = '4f27ebac5241b434a7ec45c3e1d935d0bdb2de36cc4ba97dcb63f4775542298ac790557d5e4b1f4bd8302cd03b0abd3481641dfd40887eed378e990a47ce2870'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ChemicalInventorySystem::Application.config.session_store :cookie_store, :key => '_ChemicalInventorySystem_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ChemicalInventorySystem::Application.config.session_store :active_record_store
