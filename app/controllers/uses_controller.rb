80d5e0b5351a9578d34bcfadd59b7ce771416607
superxingyu <superxingyu@gmail.com>
2012-11-09 00:31:58 -0800

    added validation for the case that inventory amount is less than use amount

--- app/controllers/uses_controller.rb
+++ app/controllers/uses_controller.rb
@@ -1,12 +1,20 @@
 class UsesController < ApplicationController
   def create
     @chemical = Chemical.find(params[:chemical_id])
-    @use = @chemical.uses.create(params[:use])
     
-    # reduce chemical amount according to use
-    @chemical.amount = @chemical.amount - @use.amount
-    @chemical.save
+    use_data = params[:use]
+    use_amount = use_data[:amount].to_i
     
-    redirect_to chemical_path(@chemical)
+    if @chemical.amount < use_amount
+      redirect_to chemical_path(@chemical), :notice => "Amount is not enough."
+    else
+      @use = @chemical.uses.create(use_data)
+      
+      # update chemical amount after use
+      @chemical.amount = @chemical.amount - @use.amount
+      @chemical.save
+      
+      redirect_to chemical_path(@chemical)
+    end
   end
 end
