204885e8c75e0c719b8b357f45d702ff7d618209
superxingyu <superxingyu@gmail.com>
2012-11-08 23:38:33 -0800

    added validation for use

--- app/models/use.rb
+++ app/models/use.rb
@@ -1,4 +1,8 @@
 class Use < ActiveRecord::Base
   belongs_to :chemical
   attr_accessible :amount, :chemist
+  
+  validates :chemist, :presence => true
+  validates :amount,  :presence => true,
+                      :numericality => { :greater_than_or_equal_to => 0 }
 end
