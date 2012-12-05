cae5a1b7c6545715bc9836ea856fd8e412ff054c
superxingyu <superxingyu@gmail.com>
2012-12-05 01:40:29 -0800

    added ran out date forcast

--- app/models/chemical.rb
+++ app/models/chemical.rb
@@ -54,7 +54,7 @@ class Chemical < ActiveRecord::Base
     "Chemical: #{self.name}--#{self.cas} (#{self.amount})"
   end
   
-  def calculate_actual_amount
+  def calculate_actual_amount(allow_negative=false)
     # To get actual inventory amount, consider recuring usages on top of the
     # number stored in database.
     # For each scheduled recurring use on this chemical,
@@ -65,11 +65,80 @@ class Chemical < ActiveRecord::Base
       total_deduct += ru.cumulated_consumption
     end
     actual_amount = self.amount - total_deduct
-    return actual_amount
+    return (allow_negative || actual_amount > 0) ? actual_amount : 0
+  end
+  
+  def ran_out_date_s
+    days = self.days_till_ran_out
+    if days >= 0
+      return (Time.now.to_date + days).to_s
+    else
+      return "n/a"
+    end
+  end
+  
+  def days_till_ran_out
+    dts = self.days_till_shortage
+    case dts
+    when 0 # hit shortage today
+      return -2 # already ran out
+    when -2 # hit shortage before today
+      return -2 # already ran out
+    when -1 # never will hit shortage
+      return -1 # never gonna hit ran out
+    else
+      return dts - 1
+    end
   end
 
-  def forecast_run_out_date
+  def days_till_shortage
+    # calculate weekly consumption
+    total_weekly_consumption = 0
+    self.recurring_uses.each do |ru|
+      total_weekly_consumption += ru.weekly_consumption
+    end
     
+    if total_weekly_consumption == 0
+      return -1 # never gonna hit shortage
+    end
+    
+    # this remaining amount includes today's consumptions
+    current_amount = self.calculate_actual_amount(true)
+    if current_amount < 0
+      return -2 # hit shortage already (before today)
+    end
+    
+    # number of weeks that we're good to cover
+    weeks = current_amount / total_weekly_consumption
+
+    # find the ran out day
+    remaining = current_amount
+    days = 1
+    self.daily_consumptions_for_following_week.each do |daily_consumption|
+      remaining -= daily_consumption
+      if remaining < 0:
+        break
+      end
+      days += 1
+    end
+    
+    return 7 * weeks + days
+  end
+  
+  def daily_consumptions_for_following_week
+    # for the following week (starting tomorrow)
+    # calculate daily consumption schedule
+    schedule = Array.new(7, 0)
+    self.recurring_uses.each do |ru|
+      if ru.periodicity == "daily"
+        schedule.map! {|daily_consumption| daily_consumption += ru.amount}
+      end
+      if ru.periodicity == "weekly"
+        schedule[ru.days_till_next_consumption - 1] += ru.amount
+      end  
+    end
+    logger.debug schedule
+    return schedule
   end
     
 end
--- app/models/recurring_use.rb
+++ app/models/recurring_use.rb
@@ -28,5 +28,23 @@ class RecurringUse < ActiveRecord::Base
     end
     return times * self.amount
   end
-        
+  
+  def weekly_consumption
+    # Calculate the amount that supposed to be consumed within a week
+    if self.periodicity == "daily"
+      return 7 * self.amount
+    elsif self.periodicity == "weekly"
+      return self.amount
+    end
+  end
+   
+  def days_till_next_consumption
+    if self.periodicity == "daily"
+      return 1
+    elsif self.periodicity == "weekly"
+      passed_days = (Time.now.to_date - self.created_at.to_date) % 7
+      return (7 - passed_days)
+    end   
+  end
+       
 end
