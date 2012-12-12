165327dd56c935e73c036aac00829d49c1e5efc3
superxingyu <superxingyu@gmail.com>
2012-12-11 20:30:17 -0800

    add more tests for recurring_uses_controller

--- test/functional/recurring_uses_controller_test.rb
+++ test/functional/recurring_uses_controller_test.rb
@@ -8,7 +8,27 @@ class RecurringUsesControllerTest < ActionController::TestCase
   
   test "should create recurring use" do
     assert_difference('RecurringUse.count') do
-      post :create, :recurring_use => { :chemist => "xingyu", :amount => 1000, :periodicity => "daily" }, :chemical_id => @chemical
+      post :create, :recurring_use => { :chemist => "Xingyu", :amount => 1000, :periodicity => "daily" }, :chemical_id => @chemical
+    end
+    assert_redirected_to chemical_path(assigns(:chemical))
+    
+    assert_difference('RecurringUse.count') do
+      post :create, :recurring_use => { :chemist => "Xingyu", :amount => 1000, :periodicity => "weekly", 
+        :first_effective_date => "2012-08-08", :end_date => "2012-09-08"}, :chemical_id => @chemical
+    end
+    assert_redirected_to chemical_path(assigns(:chemical))
+  end
+  
+  test "should not create recurring use if end date format is wrong" do
+    assert_no_difference('RecurringUse.count') do
+      post :create, :recurring_use => { :chemist => "Xingyu", :amount => 120, :periodicity => "weekly", 
+        :first_effective_date => "2012-08-08", :end_date => "2012090"}, :chemical_id => @chemical
+    end
+    assert_redirected_to chemical_path(assigns(:chemical))
+    
+    assert_no_difference('RecurringUse.count') do
+      post :create, :recurring_use => { :chemist => "Xingyu", :amount => 120, :periodicity => "weekly", 
+        :first_effective_date => "2012-08-08", :end_date => "AAAA"}, :chemical_id => @chemical
     end
     assert_redirected_to chemical_path(assigns(:chemical))
   end
