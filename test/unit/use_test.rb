1456a7e7ef6122bf66a8a97ebc0bd71991d0ad4f
superxingyu <superxingyu@gmail.com>
2012-12-11 18:07:00 -0800

    add test for use callback method

--- test/unit/use_test.rb
+++ test/unit/use_test.rb
@@ -3,27 +3,37 @@ require 'test_helper'
 class UseTest < ActiveSupport::TestCase
   
   test "should not save use without chemist" do
-    use = Use.new(:chemist => nil, :amount => 50)
+    @chemical = chemicals(:one)
+    use = @chemical.uses.create(:chemist => nil, :amount => 50)
     assert !use.save, "Saved the use without a chemist."
   end
   
   test "should not save use without amount" do
-    use = Use.new(:chemist => "Xingyu", :amount => nil)
+    @chemical = chemicals(:one)
+    use = @chemical.uses.create(:chemist => "Xingyu", :amount => nil)
     assert !use.save, "Saved the use without an amount."
   end
   
   test "should not save use if amount less than or equal to 0" do
-    use = Use.new(:chemist => "Xingyu", :amount => 0)
-    assert !use.save, "Saved the use with an amount of 0."
-    use = Use.new(:chemist => "Xingyu", :amount => -1)
-    assert !use.save, "Saved the use with an amount less than 0."
+    @chemical = chemicals(:one)
+    use1 = @chemical.uses.create(:chemist => "Xingyu", :amount => 0)
+    assert !use1.save, "Saved the use with an amount of 0."
+    use2 = @chemical.uses.create(:chemist => "Xingyu", :amount => -1)
+    assert !use2.save, "Saved the use with an amount less than 0."
   end
   
   test "should not save if amount is larger than inventory" do
-    chemical = Chemical.new(:name => "Formaldehyde", :cas => "50-00-0", :amount => 500)
-    chemical.save
-    use = chemical.uses.create(:chemist => "Xingyu", :amount => 600)
+    @chemical = chemicals(:one)
+    use = @chemical.uses.create(:chemist => "Xingyu", :amount => 5200)
     assert !use.save, "Saved the use with an amount larger than inventory."
   end
-  
+    
+  describe "deduct_chemical_amount_after_use" do
+    it "should deduct used amount" do
+      chemical = Chemical.new(:name => "Formaldehyde", :cas => "50-00-0", :amount => 500)
+      chemical.uses.create(:chemist => "Xingyu", :amount => 50)
+      use.chemical.should_receive(:update_attributes)
+    end
+  end
+    
 end
